5a99d4c94d014663c9b28d3801ed7d0b5a01fbcc
onpine <718215627@qq.com>
2021-10-10 10:01:55 +0800

    feat:加载轻提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import Axios from "axios";
 
+import { Toast } from "antd-mobile";
+
 const request = Axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   // timeout: 5000,
@@ -9,6 +11,10 @@ const request = Axios.create({
 request.interceptors.request.use(
   function (config) {
     // 在发送请求之前做些什么
+    Toast.show({
+      icon: "loading",
+      content: "加载中…",
+    });
     return config;
   },
   function (error) {
@@ -22,11 +28,13 @@ request.interceptors.response.use(
   function (response) {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
+    Toast.clear();
     return response;
   },
   function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    Toast.clear();
     return Promise.reject(error);
   }
 );
